ba20be4397021d421de1efad0e5c3e4de8c64a1a
Josh Crisostomo <joshuacr22@gmail.com>
2022-06-29 11:49:32 -0400

    cannot divide by 0

--- myscript.js
+++ myscript.js
@@ -61,10 +61,15 @@ equalsButton.addEventListener('click', doOperation);
 const add = (num1,num2) => (num1 + num2);
 const subtract = (num1, num2) => (num1 - num2);
 const multiply = (num1, num2) => (num1 * num2);
-const divide = (num1, num2) => (num1/num2); 
+const divide = (num1, num2) => {
+    if (num2 == 0) {
+        return "Cannot divide by 0!"
+    }
+    return (num1/num2); 
+}
 
 let operate = (num1, num2, operator) => {
-    console.log(num1, num2, operator);
+    console.log(num1, operator, num2);
     switch (operator) {
         case "+" :
             return add(num1, num2);
